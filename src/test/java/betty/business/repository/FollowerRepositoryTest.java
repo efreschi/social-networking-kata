a03e7f9e3a64fb6bf70146b82cc75c9184b7f419
Elisabetta Freschi <elisabetta.freschi@tndigit.it>
2021-11-20 00:28:45 +0100

    Repository per gestione followers

---
+++ src/test/java/betty/business/repository/FollowerRepositoryTest.java
@@ -0,0 +1,39 @@
+package betty.business.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import betty.business.model.UserModel;
+
+public class FollowerRepositoryTest {
+	
+	@Test
+	public void testScrivoELeggoUtentiSeguiti() {
+		FollowerRepository repo = new FollowerRepository();
+		UserModel charlie = new UserModel()
+				.setUsername("Charlie");
+		List<UserModel> seguiti = repo.findUsersByFollower(charlie);
+		assertThat(seguiti).isNullOrEmpty();
+		
+		UserModel alice = new UserModel()
+				.setUsername("Alice");	
+		
+		repo.addFollower(charlie, alice);
+		seguiti = repo.findUsersByFollower(charlie);
+		
+		assertThat(seguiti).isEqualTo(Arrays.asList(alice));
+		
+		UserModel bob = new UserModel()
+				.setUsername("Bob");	
+		
+		repo.addFollower(charlie, bob);
+		seguiti = repo.findUsersByFollower(charlie);
+		
+		assertThat(seguiti).isEqualTo(Arrays.asList(alice, bob));
+	}
+
+}
