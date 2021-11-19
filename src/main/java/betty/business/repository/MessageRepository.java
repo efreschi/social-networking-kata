c744317f8774883b1d1b387683376302f486bae8
Elisabetta Freschi <elisabetta.freschi@tndigit.it>
2021-11-19 23:10:02 +0100

    Gestione istante del messaggio

--- src/main/java/betty/business/repository/MessageRepository.java
+++ src/main/java/betty/business/repository/MessageRepository.java
@@ -4,13 +4,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 import betty.business.model.MessageModel;
+import betty.business.time.Clock;
 
 public class MessageRepository {
 	
 	private static final Set<MessageModel> messaggi = new HashSet<MessageModel>();
 	
-	public void save(MessageModel messagge) {
-		messaggi.add(messagge);
+	private Clock clock;
+
+	public MessageRepository(Clock clock) {
+		super();
+		this.clock = clock;
+	}
+	
+	public void save(MessageModel message) {
+		message.setTime(clock.nowDateTime());
+		messaggi.add(message);
 	}
 
 }
