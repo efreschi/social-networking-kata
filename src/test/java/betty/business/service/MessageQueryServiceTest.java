abdefbad79f6b18168e51bcad0835a86a707b85c
Elisabetta Freschi <elisabetta.freschi@tndigit.it>
2021-11-20 01:15:01 +0100

    Test wall

--- src/test/java/betty/business/service/MessageQueryServiceTest.java
+++ src/test/java/betty/business/service/MessageQueryServiceTest.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.Test;
 
 import betty.business.model.MessageModel;
 import betty.business.model.UserModel;
+import betty.business.repository.FollowerRepository;
 import betty.business.repository.MessageRepository;
 import betty.business.support.MessageModelBuilder;
 
 public class MessageQueryServiceTest {
 	
 	private MessageRepository mr = mock(MessageRepository.class);
+	private FollowerRepository fr = mock(FollowerRepository.class);
 	
 	@Test
 	public void testReadMessagesByUser() {
@@ -31,5 +33,23 @@ public class MessageQueryServiceTest {
 		List<MessageModel> messagesByUser = mqs.read(u);
 		
 		assertThat(messagesByUser).isEqualTo(messagesByUserExpected);
+	}
+
+	@Test
+	public void testWallUser() {
+		MessageQueryService mqs = new MessageQueryService(mr, fr);
+		
+		UserModel alice = new UserModel().setUsername("Alice");
+		MessageModel message1 = MessageModelBuilder.buildMessage(alice.getUsername(), "Messaggio 1");
+		MessageModel message2 = MessageModelBuilder.buildMessage(alice.getUsername(), "Messaggio 2");
+		UserModel bob = new UserModel().setUsername("Bob");
+		MessageModel messageBob = MessageModelBuilder.buildMessage(bob.getUsername(), "Messaggio 1");
+		List<MessageModel> messagesByUserExpected = Arrays.asList(messageBob, message2, message1);
+		when(fr.findUsersByFollower(bob)).thenReturn(Arrays.asList(bob, alice));
+		when(mr.findByUsers(Arrays.asList(bob, alice))).thenReturn(messagesByUserExpected);
+		
+		List<MessageModel> wall = mqs.wall(bob);
+		
+		assertThat(wall).isEqualTo(messagesByUserExpected);
 	}
 }
